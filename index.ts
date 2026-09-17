e2d1771c8f6a1b4ee6be7bb3fd5a84699941e343
nikinikiannyy33-sudo <nikinikiannyy33@gmail.com>
2026-09-17 23:50:51 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -174,7 +174,7 @@ let maxLosingStreak = 0;
     const nextCandle = candles[i + 1];
 if (!nextCandle) continue;
     const exit = nextCandle.close;
-const stopLoss = 4.9;
+const stopLoss = 4.8;
     let pips =
       signal === "BUY"
         ? (exit - entry) * 100
