44b4d98eb9cc0d9d872fc241cea8a718931bd870
nikinikiannyy33-sudo <nikinikiannyy33@gmail.com>
2026-09-17 17:58:47 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -158,9 +158,9 @@ let maxLosingStreak = 0;
 
     let signal = "WAIT";
 
-    if (sma5 > sma10 && rsi14 < 70) {
+    if (sma5 > sma10 && rsi14 < 60) {
       signal = "BUY";
-    } else if (sma5 < sma10 && rsi14 > 30) {
+    } else if (sma5 < sma10 && rsi14 > 40) {
       signal = "SELL";
     }
 
